b03e3f95cbc94599ada3087554c7e7e855cc4e6e
György Császár <gyoergy.csaszar@elpro.com>
2023-06-07 13:18:08 +0200

    new textbox tests

--- ToolsQA.Tests/Elements/TextBoxTests.cs
+++ ToolsQA.Tests/Elements/TextBoxTests.cs
@@ -1,5 +1,7 @@
-﻿using ToolsQA.Tests.Common;
+﻿using OpenQA.Selenium;
+using ToolsQA.Tests.Common;
 using ToolsQA.Tests.Definitions;
+using ToolsQA.Tests.Extensions;
 using ToolsQA.Tests.Pages;
 
 namespace ToolsQA.Tests.Elements
@@ -20,14 +22,43 @@ namespace ToolsQA.Tests.Elements
         }
 
         [Test]
+        [TestCase("")]
+        public void InputFullName_Negative(string value)
+        {
+            _textBoxPage.InputFullName(value);
+            _textBoxPage.ClickSubmitButton();
+            Assert.That(_textBoxPage.OutputDiv.IsElementPresent(By.Id("name")), Is.False);
+        }
+
+        [Test]
         [TestCase("John Smith")]
         [TestCase("Jane Smith")]
         [TestCase("This is a very long full name just like people have in brasil")]
-        public void InputFullName(string value)
+        public void InputFullName_Positive(string value)
         {
             _textBoxPage.InputFullName(value);
             _textBoxPage.ClickSubmitButton();
             Assert.That(_textBoxPage.NameP.Displayed);
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase("invalidemail.com")]
+        [TestCase("invalid@emailcom")]
+        public void InputEmail_Negative(string value)
+        {
+            _textBoxPage.InputEmail(value);
+            _textBoxPage.ClickSubmitButton();
+            Assert.That(_textBoxPage.OutputDiv.IsElementPresent(By.Id("email")), Is.False);
+        }
+
+        [Test]
+        [TestCase("valid@email.com")]
+        public void InputEmail_Positive(string value)
+        {
+            _textBoxPage.InputEmail(value);
+            _textBoxPage.ClickSubmitButton();
+            Assert.That(_textBoxPage.EmailP.Displayed);
         }
     }
 }
---
+++ ToolsQA.Tests/Extensions/ISearchContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+
+namespace ToolsQA.Tests.Extensions
+{
+    internal static class ISearchContextExtensions
+    {
+        internal static bool TryFindElement(this ISearchContext searchContext, By by, out IWebElement result)
+        {
+            try
+            {
+                result = searchContext.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                result = null!;
+                return false;
+            }
+        }
+
+        internal static bool IsElementPresent(this ISearchContext searchContext, By by)
+        {
+            return searchContext.TryFindElement(by, out _);
+        }
+    }
+}
