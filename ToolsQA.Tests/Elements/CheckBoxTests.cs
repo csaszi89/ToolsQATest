a72921362364c1943f9621682b90a52abac338e0
György Császár <gyoergy.csaszar@elpro.com>
2023-06-08 11:24:20 +0200

    new test for check boxes

---
+++ ToolsQA.Tests/Elements/CheckBoxTests.cs
@@ -0,0 +1,29 @@
+﻿using ToolsQA.Tests.Common;
+using ToolsQA.Tests.Definitions;
+using ToolsQA.Tests.Pages;
+
+namespace ToolsQA.Tests.Elements
+{
+    public class CheckBoxTests : TestBase
+    {
+        private CheckBoxPage _checkBoxPage;
+
+        public CheckBoxTests(BrowserInfo browserInfo) : base(browserInfo)
+        {
+        }
+
+        [SetUp]
+        public void TextBoxTestsSetUp()
+        {
+            _checkBoxPage = new CheckBoxPage(Driver);
+            _checkBoxPage.NavigateTo();
+        }
+
+        [Test]
+        public void ToggleHomeCheckBox()
+        {
+            _checkBoxPage.ToggleHomeCheckBox();
+            Assert.That(_checkBoxPage.IsResultPresent("home"));
+        }
+    }
+}
---
+++ ToolsQA.Tests/Pages/CheckBoxPage.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using ToolsQA.Tests.Common;
+using ToolsQA.Tests.Extensions;
+
+namespace ToolsQA.Tests.Pages
+{
+    public class CheckBoxPage : PageBase
+    {
+        public CheckBoxPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        protected override string Url => $"{BaseUrl}/checkbox";
+
+        private IWebElement CheckBoxTree => _driver.FindElement(By.Id("tree-node"));
+
+        private IWebElement HomeListItem => CheckBoxTree.FindElement(By.XPath("ol/li"));
+
+        private IWebElement HomeExpandCollapseButton => HomeListItem.FindElement(By.XPath("span/button"));
+
+        private IWebElement HomeCheckBox => HomeListItem.FindElement(By.Id("tree-node-home"));
+
+        private IWebElement ResultDiv => _driver.FindElement(By.Id("result"));
+
+        public void ToggleHomeExpandCollapseButton()
+        {
+            HomeExpandCollapseButton.Click();
+        }
+
+        public void ToggleHomeCheckBox()
+        {
+            var executor = (IJavaScriptExecutor)_driver;
+            executor.ExecuteScript("arguments[0].click();", HomeCheckBox);
+        }
+
+        public bool IsResultPresent(string text)
+        {
+            return ResultDiv.TryFindElement(By.XPath($"span[text()='{text}']"), out _);
+        }
+    }
+}
